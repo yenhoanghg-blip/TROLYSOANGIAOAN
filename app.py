7715ede77525b50d0c0bde331de282e73b509522
yenhoanghg-blip <yenhoang.hg@gmail.com>
2025-12-10 11:06:05 +0700

    Update footer information in app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Hoàng Văn Yến - Trường PTDTBT Tiểu học A Thượng Sơn- ĐT: 085 9456 538</div>", unsafe_allow_html=True)
