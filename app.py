95ebf8a287ba03e823c797392f400d46dab83bc1
yenhoanghg-blip <yenhoang.hg@gmail.com>
2025-12-10 11:10:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Hoàng Văn Yến - Trường PTDTBT Tiểu học A Thượng Sơn - ĐT: 085 9456 538</p>
 </div>
 """, unsafe_allow_html=True)
 
